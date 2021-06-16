fad50ae2fb4b6c42bc5a7b554b6bed748290f780
Huy <huytqth2008010@fpt.edu.vn>
2021-06-16 11:34:21 -0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebController;
+use App\Http\Controllers\CategoryController;
+use \App\Http\Controllers\ProductController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[WebController::class,"home"]);
+Route::get('/about-us',[WebController::class,"aboutUs"]);
+
+Route::get('/categories',[CategoryController::class,"all"]);
+Route::get('/categories/new',[CategoryController::class,"form"]);
+Route::post('/categories/save',[CategoryController::class,"save"]);
+Route::get('/categories/edit/{id}',[CategoryController::class,"edit"]);
+Route::post('/categories/update/{id}',[CategoryController::class,"update"]);
+
+Route::get('/products',[ProductController::class,"all"]);
+Route::get('/products/new',[ProductController::class,"form"]);
+Route::post('/products/save',[ProductController::class,"save"]);
